7f271f58d2538b7cee54fd9e98ef70aacb1cb3d4
shucshin <robinucscs@gmail.com>
2023-04-21 06:24:57 -0600

    Update Apr21

---
+++ DynamicRangeSumQueries.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+#define rep(i,a,b) for(int i = a; i < b; i++)
+
+// Segment Tree :: vll a(n), segtree(4*n); v=1, tl=0, tr=n-1;
+void build(vll &a, vll &t, int v, int tl, int tr) {
+    if(tl==tr) t[v] = a[tl]; 
+    else {
+        int m = (tl+tr)/2; 
+        build(a,t,v*2,tl,m); build(a,t,v*2+1,m+1,tr); 
+        t[v] = t[v*2]+t[v*2+1];
+    }
+}
+
+void update(vll &t, int v, int tl, int tr, int pos, ll x) {
+    if(tl==tr) t[v]=x; 
+    else {
+        int m = (tl+tr)/2; 
+        (pos<=m) ? update(t,v*2,tl,m,pos,x) : update(t,v*2+1,m+1,tr,pos,x); 
+        t[v]=t[v*2]+t[v*2+1];
+    }
+}
+
+ll RSQ(vll &t, int v, int tl, int tr, int l , int r) {
+    if(tl>r || tr<l) return 0; 
+    if(l<=tl && r>=tr) return t[v]; 
+    int m = (tl+tr)/2; 
+    return RSQ(t,v*2,tl,m,l,r) + RSQ(t,v*2+1,m+1,tr,l,r);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, q, o; cin >> n >> q;
+    vll v(n), segtree(n*4);
+    rep(i,0,n) cin >> v[i];
+    // 2 ^ log_2(n) < 4 * n
+    build(v,segtree,1,0,n-1);
+    while(q--) {
+        cin >> o;
+        if(o == 1) {
+            int k; ll u; cin >> k >> u; k--;
+            update(segtree,1,0,n-1,k,u);
+        } else {
+            int a, b; cin >> a >> b; a--; b--;
+            cout << RSQ(segtree,1,0,n-1,a,b) << "\n";
+        }   
+    }
+}
